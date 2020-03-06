ff30035cb5d86b95cbdc4d697446c4d5c156a6c0
jashna14 <jashn.arora@research.iiit.ac.in>
2020-03-06 21:59:34 +0530

    another solution added

--- Assignment-2/part_11.py
+++ Assignment-2/part_11.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 # U(t+1)(i) = max_A[R(i,A) + gamma * SIGMA [P(j| i,A) * U(t)(j)]]
-# P(t+1)(i) = argmax_A[R(i,A) + gamma * SIGMA [P(j| i,A) * U(t+1)(j)]]
 
 Penalty = -5
 Gamma = 0.99
@@ -69,40 +68,40 @@ def utility_shoot(Uprev , MD_health , arrows_cnt , hero_stamina):
     prob_hit = 0.5
 
     if (arrows_cnt > 0 and hero_stamina > 0):
-        return (reward_shoot(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_hit*Uprev[MD_health-1][arrows_cnt-1][hero_stamina-1] + (1-prob_hit)*Uprev[MD_health][arrows_cnt-1][hero_stamina-1]))
+        return (reward_shoot(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_hit*Ucurr[MD_health-1][arrows_cnt-1][hero_stamina-1] + (1-prob_hit)*Ucurr[MD_health][arrows_cnt-1][hero_stamina-1]))
 
 def utility_dodge(Uprev, MD_health , arrows_cnt , hero_stamina):
     
     if (hero_stamina == 2 and arrows_cnt < 3):
         prob_stamina_reduce_by_50 = 0.8
         prob_pick_arrow = 0.8
-        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*prob_pick_arrow*Uprev[MD_health][arrows_cnt+1][hero_stamina-1] + prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-1] + (1-prob_stamina_reduce_by_50)*prob_pick_arrow*Uprev[MD_health][arrows_cnt+1][hero_stamina-2] + (1-prob_stamina_reduce_by_50)*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-2]))          
+        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*prob_pick_arrow*Ucurr[MD_health][arrows_cnt+1][hero_stamina-1] + prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-1] + (1-prob_stamina_reduce_by_50)*prob_pick_arrow*Ucurr[MD_health][arrows_cnt+1][hero_stamina-2] + (1-prob_stamina_reduce_by_50)*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-2]))          
 
     elif (hero_stamina == 2 and arrows_cnt == 3):
         prob_pick_arrow = 0
         prob_stamina_reduce_by_50 = 0.8
-        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-1] + (1-prob_stamina_reduce_by_50)*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-2]))          
+        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-1] + (1-prob_stamina_reduce_by_50)*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-2]))          
 
     elif (hero_stamina == 1 and arrows_cnt < 3):
         prob_stamina_reduce_by_50 = 1
         prob_pick_arrow = 0.8
-        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*prob_pick_arrow*Uprev[MD_health][arrows_cnt+1][hero_stamina-1] + prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-1]))           
+        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*prob_pick_arrow*Ucurr[MD_health][arrows_cnt+1][hero_stamina-1] + prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-1]))           
 
     elif (hero_stamina ==1 and arrows_cnt == 3):
         prob_stamina_reduce_by_50 = 1
         prob_pick_arrow = 0  
-        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Uprev[MD_health][arrows_cnt][hero_stamina-1]))
+        return (reward_dodge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_stamina_reduce_by_50*(1-prob_pick_arrow)*Ucurr[MD_health][arrows_cnt][hero_stamina-1]))
 
 
 def utility_recharge(Uprev, MD_health , arrows_cnt , hero_stamina):
 
     if (hero_stamina == 2):
         prob_recharge = 0
-        return (reward_recharge(MD_health , arrows_cnt , hero_stamina) + Gamma*((1-prob_recharge)*Uprev[MD_health][arrows_cnt][hero_stamina]))
+        return (reward_recharge(MD_health , arrows_cnt , hero_stamina) + Gamma*((1-prob_recharge)*Ucurr[MD_health][arrows_cnt][hero_stamina]))
     
     elif (hero_stamina < 2):
         prob_recharge = 0.8
-        return (reward_recharge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_recharge*Uprev[MD_health][arrows_cnt][hero_stamina+1] + (1-prob_recharge)*Uprev[MD_health][arrows_cnt][hero_stamina]))
+        return (reward_recharge(MD_health , arrows_cnt , hero_stamina) + Gamma*(prob_recharge*Ucurr[MD_health][arrows_cnt][hero_stamina+1] + (1-prob_recharge)*Ucurr[MD_health][arrows_cnt][hero_stamina]))
 
 def policy_shoot(Ucurr, MD_health , arrows_cnt , hero_stamina):
 
@@ -165,26 +164,8 @@ def print_iteration(Ucurr , Policy , iteration_cnt):
             for k in range(max_hero_stamina + 1):
                 print('(' + str(i) + ',' + str(j) + ',' + str(k) + ')' + ':' + str(Policy[i][j][k]) + '=' + '[' + str(Ucurr[i][j][k]) + ']')        
 
-def value_iteration(Uprev , Ucurr , iteration_cnt):
+def value_iteration(Ucurr , iteration_cnt):
     
-    for i in range (max_MD_health + 1):
-        for j in range(max_arrows_cnt + 1):
-            for k in range(max_hero_stamina + 1):
-                if (i > 0):
-                    if(can_dodge(i,j,k) and can_shoot(i,j,k)):
-                        Ucurr[i][j][k] = max(utility_shoot(Uprev,i,j,k) , utility_dodge(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
-
-                    elif(can_shoot(i,j,k)):
-                        Ucurr[i][j][k] = max(utility_shoot(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
-
-                    elif(can_dodge(i,j,k)):
-                        Ucurr[i][j][k] = max(utility_dodge(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
-
-                    else:
-                        Ucurr[i][j][k] = utility_recharge(Uprev,i,j,k)
-                else:
-                    Ucurr[i][j][k] = 0
-
     for i in range (max_MD_health + 1):
         for j in range(max_arrows_cnt + 1):
             for k in range(max_hero_stamina + 1):
@@ -198,6 +179,7 @@ def value_iteration(Uprev , Ucurr , iteration_cnt):
                             Policy[i][j][k] = 'DODGE'
                         elif(index == 2):
                             Policy[i][j][k] = 'RECHARGE'
+                        Ucurr[i][j][k] = max(utility_shoot(Uprev,i,j,k) , utility_dodge(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
 
                     elif(can_shoot(i,j,k)):
                         policy = [policy_shoot(Ucurr,i,j,k),policy_recharge(Ucurr,i,j,k)]
@@ -206,6 +188,7 @@ def value_iteration(Uprev , Ucurr , iteration_cnt):
                             Policy[i][j][k] = 'SHOOT'
                         if(index == 1):
                             Policy[i][j][k] = 'RECHARGE'
+                        Ucurr[i][j][k] = max(utility_shoot(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
 
                     elif(can_dodge(i,j,k)):
                         policy = [policy_dodge(Ucurr,i,j,k),policy_recharge(Ucurr,i,j,k)]
@@ -214,10 +197,14 @@ def value_iteration(Uprev , Ucurr , iteration_cnt):
                             Policy[i][j][k] = 'DODGE'
                         elif(index == 1):
                             Policy[i][j][k] = 'RECHARGE'
+                        Ucurr[i][j][k] = max(utility_dodge(Uprev,i,j,k) , utility_recharge(Uprev,i,j,k))
+
                     else:
-                        Policy[i][j][k] = 'RECHARGE'    
+                        Policy[i][j][k] = 'RECHARGE'
+                        Ucurr[i][j][k] = utility_recharge(Uprev,i,j,k)
                 else:
-                    Policy[i][j][k] = '-1'                
+                    Policy[i][j][k] = '-1'
+                    Ucurr[i][j][k] = 0                
 
     print_iteration(Ucurr , Policy , iteration_cnt)
 
@@ -251,7 +238,7 @@ while(1):
     else:
         iteration_cnt += 1
         update(Uprev,Ucurr)
-        value_iteration(Uprev,Ucurr,iteration_cnt)    
+        value_iteration(Ucurr,iteration_cnt)    
 
 
 
